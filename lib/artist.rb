7bc3cec63606476fbbc9ae12063278c850a05fe7
Mark Hernandez <m.hern1415@gmail.com>
2019-05-17 22:56:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,21 @@
-class Artist 
-  attr_accessor :name, :songs 
-  
-  @@song_count = 0 
-  
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
-  
-    def add_song_by_name(name)
+
+  def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
@@ -24,7 +25,5 @@ class Artist
   def self.song_count
     @@song_count
   end
-  
 
 end
-
